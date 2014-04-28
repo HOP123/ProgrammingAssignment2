652286b5a43fbbc5298090b73cc2a753322ab698
HOP123 <hisayo-powell@comcast.net>
2014-04-27 19:29:18 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,9 +3,51 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+makeCacheMatrix <- function(x) {
+	#x <- matrix
+	x <- x
+	y <- matrix()
+	z <- matrix()
+	
+	## set() set matrix, if matrix is passed, update the stored matris. 
+	## If emply argument is passed, keep the last marix 
+	set <- function (y=x) {
+		x <<- y
+		#print(x)
+	}
+	## get functin prints the matrix
+	print(x)
+	get <- function(y=x){
+		print("In get")
+# 		print(class(x))
+# 		print(nrow(x))
+ 		if (nrow(x) == ncol(x)){
+ 			return (x)
+		}
+ 		else {print("This matrix cannot be inverted.")}	
+	}
+	
+	## setInverse function calculates inverse matrix 
+	setInverse <- function(y=x){
+		print("came to setInverse")
+		if(nrow(y) == ncol(y)){
+			z <<- solve(y)
+			print(z)
+		}
+		else {print("No inverse")}
+	}
+	
+	## getInverse function prints the inverse matrix
+	getInverse <- function(){
+		print("came to getInverse")
+		print(z)
+		return (z)
+	}
+	return (list(set=set, get=get, setInverse=setInverse, getInverse=getInverse))
 }
+
+
 
 
 ## Write a short comment describing this function
